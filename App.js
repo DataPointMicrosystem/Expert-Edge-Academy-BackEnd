c81b7e5ade337560cbe81a915511c9eaff82037b
ColdDdev <colindecorce@gmail.com>
2026-09-12 11:56:27 +0100

    Workig

--- App.js
+++ App.js
@@ -26,12 +26,7 @@ const mediaRouter = require("./routes/mediaRouter");
 const app = express();
 app.disable("x-powered-by");
 app.use(helmet());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:8443",
-    credentials: true,
-  }),
-);
+app.use(cors({ origin: "*" }));
 app.use("/api/payments/webhook", express.raw({ type: "application/json" }));
 app.use(express.json({ limit: "1mb" }));
 app.use(
